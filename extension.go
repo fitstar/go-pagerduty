2dae9b44a3287eca21b0fef5f9b2486c5f80c262
Nika Jones <njones@fitbit.com>
2019-02-01 14:40:31 -0800

    Added extensions functions to the code with array in the proper place with a map

--- extension.go
+++ extension.go
@@ -10,10 +10,10 @@ import (
 // Extension is a third-party extension to PagerDuty's API.
 type Extension struct {
 	APIObject
-	Name             string      `json:"name,omitempty"`
-	EndpointURL      string      `json:"endpoint_url,omitempty"`
-	ExtensionSchema  APIObject   `json:"extension_schema,omitempty"`
-	ExtensionObjects []APIObject `json:"extension_objects,omitempty"`
+	Name             string                 `json:"name,omitempty"`
+	EndpointURL      string                 `json:"endpoint_url,omitempty"`
+	ExtensionSchema  map[string]interface{} `json:"extension_schema,omitempty"`
+	ExtensionObjects []APIObject            `json:"extension_objects,omitempty"`
 }
 
 // ListExtensionOptions are the options available when calling the ListExtensions API endpoint.
